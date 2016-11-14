6ea17eb605817c12a455f0e6af05139b23802d17
Ken Erwin <ken@devopslibrary.com>
2016-11-14 17:48:19 -0500

    New version of contract

--- PixelMap.sol
+++ PixelMap.sol
@@ -1,65 +1,69 @@
 pragma solidity ^0.4.2;
 contract PixelMap {
-    mapping (uint => address) public owners;
-    mapping (uint => string) public images;
-    mapping (uint => string) public urls;
-    mapping (uint => uint) public prices;
     address creator;
-    event TileUpdated(uint x, uint y);
+    struct Tile {
+        address owner;
+        string image;
+        string url;
+        uint price;
+    }
+    mapping (uint => Tile) public tiles;
+    event TileUpdated(uint location);
 
-    // Constructor
-    function PixelMap() {
-        creator = msg.sender;
-    }
-    // Given X & Y, return Tile number.
-    function getPos(uint x, uint y) returns (uint) {
-        return y*81+x;
-    }
+    // Original Tile Owner
+    function PixelMap() {creator = msg.sender;}
 
     // Get Tile information at X,Y position.
-    function getTile(uint x, uint y) returns (address, string, string, uint) {
-        uint location = getPos(x, y);
-        return (owners[location], urls[location], images[location], prices[location]);
+    function getTile(uint location) returns (address, string, string, uint) {
+        if (location > 3969) {throw;}
+        if (location < 0) {throw;}
+        return (tiles[location].owner,
+                tiles[location].image,
+                tiles[location].url,
+                tiles[location].price);
     }
 
-    // Purchase an unclaimed Tile for 10 Eth.
-    function buyTile(uint x, uint y) payable {
-        uint location = getPos(x, y);
-        uint price;
+    // Purchase an unclaimed Tile for 2 Eth.
+    function buyTile(uint location) payable {
+        uint price = tiles[location].price;
         address owner;
 
-        if (owners[location] == msg.sender) {
-            throw; // You already own this pixel silly!
+        // Make sure person doesn't already own tile.
+        if (tiles[location].owner == msg.sender) {
+            throw;
         }
-        // If Unowned by the Bank
-        if (owners[location] == 0x0) {
+
+        // If Unowned by the Bank, sell for 2Eth.
+        if (tiles[location].owner == 0x0) {
             price = 2000000000000000000;
             owner = creator;
         }
 
-        // If the pixel isn't for sale, don't sell it!
+        // If the tile isn't for sale, don't sell it!
         if (price == 0) {
             throw;
         }
 
-        if (owner.send(price)) {
-            owners[location] = msg.sender;
-            prices[location] = 0; // Set Price to 0.
-            TileUpdated(x, y);
+        // Pay for Tile.
+        if (msg.value != price) {
+            throw;
+        }
+        if (msg.sender.send(price)) {
+            tiles[location].owner = msg.sender;
+            tiles[location].price = 0; // Set Price to 0.
+            TileUpdated(location);
         }
         else {throw;}
     }
 
     // Set an already owned Tile to whatever you'd like.
-    function setTile(uint x, uint y, string image, string url, uint price) {
-        uint location = getPos(x, y);
-        if (owners[location] != msg.sender) {throw;} // Pixel not owned by you!
-        if (bytes(image).length != 768) {throw;} // Incorrect String Length Provided!
+    function setTile(uint location, string image, string url, uint price) {
+        if (tiles[location].owner != msg.sender) {throw;} // Pixel not owned by you!
         else {
-            images[location] = image;
-            urls[location] = url;
-            prices[location] = price;
-            TileUpdated(x, y);
+            tiles[location].image = image;
+            tiles[location].url = url;
+            tiles[location].price = price;
+            TileUpdated(location);
         }
     }
 }
