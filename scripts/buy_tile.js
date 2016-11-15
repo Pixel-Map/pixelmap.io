5e8f06c23ff9a7768433fc6db022b95dbc83e81c
Ken Erwin <ken@devopslibrary.com>
2016-11-15 17:26:02 -0500

    Made Help Icon Clickable

--- scripts/buy_tile.js
+++ scripts/buy_tile.js
@@ -15,13 +15,13 @@ function sendEther(x, y) {
     var split = spanText.split("x");
     var location = parseInt(split[0]) + (parseInt(split[1]) * 81)
     var pixelmapContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"tiles","outputs":[{"name":"owner","type":"address"},{"name":"image","type":"string"},{"name":"url","type":"string"},{"name":"price","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"uint256"}],"name":"buyTile","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"uint256"},{"name":"image","type":"string"},{"name":"url","type":"string"},{"name":"price","type":"uint256"}],"name":"setTile","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"uint256"}],"name":"getTile","outputs":[{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"location","type":"uint256"}],"name":"TileUpdated","type":"event"}]);
-    var myContractInstance = pixelmapContract.at('0x70F8Ea91af79BDD15977a48B12A47Fdf4E23D841');
+    var myContractInstance = pixelmapContract.at('0x406D4d30dE0C7c11499aDA85a3DC50c735B6dd60');
 
     // Buy a Tile
     myContractInstance.buyTile(location, {
         from: web3.eth.defaultAccount,
         value: web3.toWei('2', 'ether'),
-        to: '0x70F8Ea91af79BDD15977a48B12A47Fdf4E23D841'
+        to: '0x406D4d30dE0C7c11499aDA85a3DC50c735B6dd60'
     }, function(error, result) {
         if (!error) {
             console.log(result)
@@ -38,14 +38,14 @@ function updateTile(x, y) {
     var split = spanText.split("x");
     var location = parseInt(split[0]) + (parseInt(split[1]) * 81)
     var pixelmapContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"tiles","outputs":[{"name":"owner","type":"address"},{"name":"image","type":"string"},{"name":"url","type":"string"},{"name":"price","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"uint256"}],"name":"buyTile","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"uint256"},{"name":"image","type":"string"},{"name":"url","type":"string"},{"name":"price","type":"uint256"}],"name":"setTile","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"uint256"}],"name":"getTile","outputs":[{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"location","type":"uint256"}],"name":"TileUpdated","type":"event"}]);
-    var myContractInstance = pixelmapContract.at('0x70F8Ea91af79BDD15977a48B12A47Fdf4E23D841');
+    var myContractInstance = pixelmapContract.at('0x406D4d30dE0C7c11499aDA85a3DC50c735B6dd60');
     var url = document.getElementById('url').value;
     var price = document.getElementById('price').value;
     // Buy a Tile
     myContractInstance.setTile(location, selectedImage, url, web3.toWei(price, 'ether'), {
         from: web3.eth.defaultAccount,
         value: 0,
-        to: '0x70F8Ea91af79BDD15977a48B12A47Fdf4E23D841'
+        to: '0x406D4d30dE0C7c11499aDA85a3DC50c735B6dd60'
     }, function(error, result) {
       if (!error) {
           console.log(result)
