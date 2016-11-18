01bb69c5250e3fa52faf6bb730804fa0c55d92cd
Ken Erwin <ken@devopslibrary.com>
2016-11-18 12:52:51 -0500

    Fixed chat :D

--- scripts/pixelmap.js
+++ scripts/pixelmap.js
@@ -1,4 +1,7 @@
 $('img[usemap]').maphilight()
+if (typeof web3 == 'undefined') {
+  console.log('hi');
+}
 web3.eth.defaultAccount=web3.eth.accounts[0]
 for (var x = 0, x; x < 81; x++) {
     for (var y = 0, y; y < 49; y++) {
