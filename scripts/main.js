1d3b3999a9517b408bd9564e4549d6c813c72be4
Smallfish <ziyi-26@hotmail.com>
2021-08-31 03:01:22 +0200

    Center map, Day/Night
    * Issue #1 - Added night map
    Issue #8 - Centered map wrapper
    
    * Just #issue 8
    
    * Issue 8 - Centering map
    
    * Issue 8 - Centering attempt 5
    
    * #Issue 8 - attempt 6
    
    * Issue #1 - day/night mode
    
    * Changed the inverted operators of isDayTime

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+function decideBackground() {
+
+    const hours = new Date().getHours();
+    const isDayTime = hours > 7 && hours < 20;
+
+    const day = "../images/bg4.jpg";
+    const night = "../images/bg4 night.jpg";
+
+    if (isDayTime) {
+        $('html').css('background-image', "url('../images/bg4.jpg')");
+    } else {
+        $('html').css('background-image', "url('../images/bg4 night.jpg')");
+    }
+}
