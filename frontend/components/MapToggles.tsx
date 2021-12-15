4479f8d5e71f426a565df5a8880c3f3e1ec91408
0xdazzle <magic@dazzle.com>
2021-12-15 12:46:10 +1100

    visible map toggles

--- frontend/components/MapToggles.tsx
+++ frontend/components/MapToggles.tsx
@@ -12,26 +12,31 @@ export default function MapToggles({
 
   return (
     <div className="flex space-x-4">
-      <label>
-        <input
-          type="checkbox"
-          className="nes-checkbox is-dark"
-          checked={showForSale}
-          onChange={(e) => setShowForSale(e.currentTarget.checked)}
-        />
-        <span className="font-bold text-sm">For Sale</span>
-      </label>
-
-      {account && account != "" && (
-        <label>
+      <span className="inline-flex items-center px-2.5 py-0.5 rounded-full bg-gray-900 bg-opacity-70 border border-white">
+        <label
+          className="my-0">
           <input
             type="checkbox"
             className="nes-checkbox is-dark"
-            checked={showOwned}
-            onChange={(e) => setShowOwned(e.currentTarget.checked)}
+            checked={showForSale}
+            onChange={(e) => setShowForSale(e.currentTarget.checked)}
           />
-          <span className="font-bold text-sm">Owned</span>
+          <span className="font-bold text-sm">For Sale</span>
         </label>
+      </span>
+      {account && account != "" && (
+        <span className="inline-flex items-center px-2.5 py-0.5 rounded-full bg-gray-900 bg-opacity-70 border border-white">
+          <label
+            className="my-0">
+            <input
+              type="checkbox"
+              className="nes-checkbox is-dark"
+              checked={showOwned}
+              onChange={(e) => setShowOwned(e.currentTarget.checked)}
+            />
+            <span className="font-bold text-sm">Owned</span>
+          </label>
+        </span>
       )}
     </div>
   );
