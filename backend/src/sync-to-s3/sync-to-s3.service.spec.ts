469bb92892ddd88c891c50263e0b4a5a97d88f2b
Ken Erwin <ken@devopslibrary.com>
2021-12-20 17:28:43 -0500

    minor fix for testing;

--- backend/src/sync-to-s3/sync-to-s3.service.spec.ts
+++ backend/src/sync-to-s3/sync-to-s3.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SyncToS3Service } from './sync-to-s3.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('SyncToS3Service', () => {
   let service: SyncToS3Service;
@@ -7,6 +8,7 @@ describe('SyncToS3Service', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [SyncToS3Service],
+      imports: [ConfigModule],
     }).compile();
 
     service = module.get<SyncToS3Service>(SyncToS3Service);
