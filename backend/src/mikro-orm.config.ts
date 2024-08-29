f2675e32530dc0676431a6ce7fa06644d31cb263
Ken Erwin <ken@pixelmap.io>
2024-08-29 15:46:03 -0400

    Starting on IAC, good bye Heroku

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -1,17 +1,29 @@
-import { DataHistory } from './ingestor/entities/dataHistory.entity';
-import { PurchaseHistory } from './ingestor/entities/purchaseHistory.entity';
-import { Tile } from './ingestor/entities/tile.entity';
-import { TransferHistory } from './ingestor/entities/transferHistory.entity';
-import { WrappingHistory } from './ingestor/entities/wrappingHistory.entity';
-import { CurrentState } from './ingestor/entities/currentState.entity';
-import { PixelMapTransaction } from './ingestor/entities/pixelMapTransaction.entity';
+import { DataHistory } from "./ingestor/entities/dataHistory.entity";
+import { PurchaseHistory } from "./ingestor/entities/purchaseHistory.entity";
+import { Tile } from "./ingestor/entities/tile.entity";
+import { TransferHistory } from "./ingestor/entities/transferHistory.entity";
+import { WrappingHistory } from "./ingestor/entities/wrappingHistory.entity";
+import { CurrentState } from "./ingestor/entities/currentState.entity";
+import { PixelMapTransaction } from "./ingestor/entities/pixelMapTransaction.entity";
 
 export default {
-  entities: [DataHistory, CurrentState, PurchaseHistory, Tile, TransferHistory, WrappingHistory, PixelMapTransaction],
-  type: 'postgresql',
-  dbName: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'password', // one of `mongo` | `mysql` | `mariadb` | `postgresql` | `sqlite`
+	entities: [
+		DataHistory,
+		CurrentState,
+		PurchaseHistory,
+		Tile,
+		TransferHistory,
+		WrappingHistory,
+		PixelMapTransaction,
+	],
+	type: "postgresql",
+	dbName: process.env.DB_NAME,
+	host: process.env.DB_HOST,
+	port: 5432,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	debug: true,
+	driverOptions: {
+		connection: { ssl: { rejectUnauthorized: false } },
+	},
 };
